8880954d279e07c934e6c62419c173d2edb6539d
somasiabdul11 <somasiabdul111@gmail.com>
2025-08-06 23:43:22 +0700

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -8,9 +8,9 @@ export async function onRequest(context) {
   const isAndroid = /Android/i.test(ua);
   const isChromeMobile = /Chrome\/\d+\.\d+ Mobile/i.test(ua);
   
-  if (!isAndroid || !isChromeMobile) {
-    return new Response("Blocked: Only Android Chrome Mobile allowed", { status: 403 });
-  }
+  // if (!isAndroid || !isChromeMobile) {
+  //   return new Response("Blocked: Only Android Chrome Mobile allowed", { status: 403 });
+  // }
 
   // 2. Block Cloudflare-detected bots
   const isBot = request.headers.get("cf-is-bot") === "true";
